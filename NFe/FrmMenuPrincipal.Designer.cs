5f95de2af677be260795ccf7aa8a1a6e30d2fe35
Júnior Theodoro <juninho_lpisrock@hotmail.com>
2019-05-28 17:34:17 -0300

    Adicionar arquivos de projeto.

---
+++ NFe/FrmMenuPrincipal.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace NFe {
+    partial class FrmMenuPrincipal {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenuPrincipal));
+            this.tabMenuPrincipal = new MaterialSkin.Controls.MaterialTabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.materialTabSelector1 = new MaterialSkin.Controls.MaterialTabSelector();
+            this.tabMenuPrincipal.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabMenuPrincipal
+            // 
+            this.tabMenuPrincipal.Controls.Add(this.tabPage1);
+            this.tabMenuPrincipal.Controls.Add(this.tabPage2);
+            this.tabMenuPrincipal.Controls.Add(this.tabPage3);
+            this.tabMenuPrincipal.Depth = 0;
+            this.tabMenuPrincipal.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabMenuPrincipal.Location = new System.Drawing.Point(12, 106);
+            this.tabMenuPrincipal.MouseState = MaterialSkin.MouseState.HOVER;
+            this.tabMenuPrincipal.Name = "tabMenuPrincipal";
+            this.tabMenuPrincipal.SelectedIndex = 0;
+            this.tabMenuPrincipal.Size = new System.Drawing.Size(776, 460);
+            this.tabMenuPrincipal.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(768, 392);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Cadastros";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(768, 392);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Vendas";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(768, 431);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Serviços";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // materialTabSelector1
+            // 
+            this.materialTabSelector1.BaseTabControl = this.tabMenuPrincipal;
+            this.materialTabSelector1.Depth = 0;
+            this.materialTabSelector1.Location = new System.Drawing.Point(0, 62);
+            this.materialTabSelector1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialTabSelector1.Name = "materialTabSelector1";
+            this.materialTabSelector1.Size = new System.Drawing.Size(800, 43);
+            this.materialTabSelector1.TabIndex = 1;
+            this.materialTabSelector1.Text = "materialTabSelector1";
+            // 
+            // FrmMenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.materialTabSelector1);
+            this.Controls.Add(this.tabMenuPrincipal);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FrmMenuPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Menu Principal";
+            this.tabMenuPrincipal.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialTabControl tabMenuPrincipal;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private MaterialSkin.Controls.MaterialTabSelector materialTabSelector1;
+    }
+}
+
---
+++ NFe/FrmMenuPrincipal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+
+namespace NFe {
+    public partial class FrmMenuPrincipal : MaterialForm {
+        public FrmMenuPrincipal() {
+            InitializeComponent();
+        }
+    }
+}
